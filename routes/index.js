c9bad4fd1a950c09e335a772c6f4bb33fc736a66
dasuiyuanhao <dasuiyuanhao@gmail.com>
2015-06-01 10:59:54 +0800

    增加node_modules

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Study Node.js' });
 });
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About', year: new Date().getFullYear(), message: 'Your application description page.' });
